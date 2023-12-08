5a73d39c76d6af4559be1a0421bd2231b055aef6
ajaykumar30071996 <ajayharish034@gmail.com>
2023-12-08 08:28:37 +0200

    adding error modal component

--- src/components/Users/AddUsers/AddUser.tsx
+++ src/components/Users/AddUsers/AddUser.tsx
@@ -2,61 +2,66 @@ import { useState } from "react";
 import Button from "../../UI/Button/Button";
 import Card from "../../UI/Card/Card";
 import AddUserStles from "./AddUser.module.css";
+import ErrorModal from "../../UI/ErrorModal/ErrorModal";
 
 const AddUser = (props: any) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+    const [enteredUsername, setEnteredUsername] = useState("");
+    const [enteredAge, setEnteredAge] = useState("");
 
-  const addUserHandler = (event: any) => {
-    event.preventDefault();
-    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-        return;
-    }
-    if (+enteredAge < 1) {
-        return;
-    }
-    console.log(enteredUsername, "--", enteredAge);
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername("");
-    setEnteredAge("");
-  };
+    const addUserHandler = (event: any) => {
+        event.preventDefault();
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+        }
+        if (+enteredAge < 1) {
+            return;
+        }
+        console.log(enteredUsername, "--", enteredAge);
+        props.onAddUser(enteredUsername, enteredAge);
+        setEnteredUsername("");
+        setEnteredAge("");
+    };
 
-  const inputChangeHandler = (input: any, value: any) => {
-    console.log(input, value);
-    if (input === "username") {
-      setEnteredUsername(value);
-    } else {
-      setEnteredAge(value);
-    }
-  };
+    const inputChangeHandler = (input: any, value: any) => {
+        console.log(input, value);
+        if (input === "username") {
+            setEnteredUsername(value);
+        } else {
+            setEnteredAge(value);
+        }
+    };
 
-  return (
-    <Card cssClass={AddUserStles.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          value={enteredUsername}
-          onChange={(event: any) =>
-            inputChangeHandler("username", event.target.value)
-          }
-          id="username"
-          type="text"
-        />
+    return (
+        <div>
+            <ErrorModal title={"an error occured"} message={"something fishy"}/>
+            <Card cssClass={AddUserStles.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input
+                        value={enteredUsername}
+                        onChange={(event: any) =>
+                            inputChangeHandler("username", event.target.value)
+                        }
+                        id="username"
+                        type="text"
+                    />
 
-        <label htmlFor="age">Age (Years)</label>
-        <input
-        value={enteredAge}
-          onChange={(event: any) =>
-            inputChangeHandler("age", event.target.value)
-          }
-          id="age"
-          type="number"
-        />
+                    <label htmlFor="age">Age (Years)</label>
+                    <input
+                        value={enteredAge}
+                        onChange={(event: any) =>
+                            inputChangeHandler("age", event.target.value)
+                        }
+                        id="age"
+                        type="number"
+                    />
 
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
-  );
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+
+        </div>
+    );
 };
 
 export default AddUser;
