6ffa4b250bce3cb3a08602b0d68ff1ab2347e52e
“ajaykumar300796” <ajayharish034@gmail.com>
2023-12-11 18:14:30 +0530

    completed

--- src/components/UI/ErrorModal/ErrorModal.tsx
+++ src/components/UI/ErrorModal/ErrorModal.tsx
@@ -5,19 +5,23 @@ import ErrorModalStyleClasses from './ErrorModal.module.css';
 
 export default function ErrorModal(props: any) {
   return (
-    <Card cssClass={ErrorModalStyleClasses.modal}>
-      <header className={ErrorModalStyleClasses.header}>
-        <h2>
-          {props.title}
-        </h2>
-      </header>
-      <div className={ErrorModalStyleClasses.content}>
-        <p>
-          {props.message}
-        </p>
-      </div>
-      <footer className={ErrorModalStyleClasses.actions}>
-        <Button>okay</Button>
-      </footer>
-    </Card>)
+    <div>
+      <div className={ErrorModalStyleClasses.backdrop} onClick={props.onConfirm}></div>
+      <Card cssClass={ErrorModalStyleClasses.modal}>
+        <header className={ErrorModalStyleClasses.header}>
+          <h2>
+            {props.title}
+          </h2>
+        </header>
+        <div className={ErrorModalStyleClasses.content}>
+          <p>
+            {props.message}
+          </p>
+        </div>
+        <footer className={ErrorModalStyleClasses.actions}>
+          <Button onClick={props.onConfirm}>okay</Button>
+        </footer>
+      </Card>
+    </div>
+  )
 }
